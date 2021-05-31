0b1c83f164ef1c4f8e8e7e4af5d0b4141b0e2882
kkin1995 <karan@akinariwala.com>
2021-05-31 08:45:32 +0530

    Implemented mean absolute deviation, variance and standard deviation

--- src/spread.py
+++ src/spread.py
@@ -1,12 +1,14 @@
-def range(*arr):
-    if type(arr) == list or type(arr) == tuple:
+from central_tendancy import mean
+
+def range_of_array(*arr):
+    if type(arr[0]) == list or type(arr[0]) == tuple:
         arr = arr[0]
 
     arr = sorted(arr)
     return arr[-1] - arr[0]
 
 def inter_quartile_range(*arr):
-    if type(arr) == list or type(arr) == tuple:
+    if type(arr[0]) == list or type(arr[0]) == tuple:
         arr = arr[0]
 
     length = len(arr)
@@ -30,6 +32,38 @@ def inter_quartile_range(*arr):
         Q3 = upper[length_upper // 2]
         return Q3 - Q1
 
+def mean_absolute_deviation(*arr):
+    if type(arr[0]) == list or type(arr[0]) == tuple:
+        arr = arr[0]
+
+    length = len(arr)
+    mean_arr = mean(arr)
+    sum_of_diffs = 0
+    for i in range(length):
+        sum_of_diffs += arr[i] - mean_arr
+    return sum_of_diffs / length
+
+def variance(*arr):
+    if type(arr[0]) == list or type(arr[0]) == tuple:
+        arr = arr[0]
+
+    length = len(arr)
+    mean_arr = mean(arr)
+    sum_of_squared_diffs = 0
+    for i in range(length):
+        sum_of_squared_diffs += (arr[i] - mean_arr) ** 2
+    return sum_of_squared_diffs / (length-1)
+
+def standard_deviation(*arr):
+    if type(arr[0]) == list or type(arr[0]) == tuple:
+        arr = arr[0]
+
+    var = variance(arr)
+    from math import sqrt
+    return sqrt(var)
+
 if __name__ == "__main__":
     arr = [11,10,11,11,11,14,8,9,8]
     print(inter_quartile_range(arr))
+    print(variance(arr))
+    print(standard_deviation(arr))
